a3ba0901fc4909ea23b6648cd88e2e5b6e58a631
CrazyMew37 <64723948+CrazyMew37@users.noreply.github.com>
2025-10-12 00:29:47 -0500

    1.2.0 Changes

--- CrazyMew37-BetterHauntedGolfCourse/mods-unpacked/CrazyMew37-BetterHauntedGolfCourse/extensions/scenes/game_floor/floor_modifiers/scripts/anomalies/floor_mod_power_out.gd
+++ CrazyMew37-BetterHauntedGolfCourse/mods-unpacked/CrazyMew37-BetterHauntedGolfCourse/extensions/scenes/game_floor/floor_modifiers/scripts/anomalies/floor_mod_power_out.gd
@@ -3,7 +3,7 @@ extends "res://scenes/game_floor/floor_modifiers/scripts/anomalies/floor_mod_pow
 # LET ME SEE SLENDERCOG JEEZ -cm37
 func modify_floor() -> void:
 	var env : Environment = game_floor.environment.environment.duplicate()
-	env.background_energy_multiplier = 0.2
+	env.background_energy_multiplier = 0.05
 	env.fog_density = 0.075
 	env.fog_enabled = true
 	game_floor.environment.environment = env
